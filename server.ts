911ba21c61bbd00d32ec9ec1fceacf4113df9175
sovitha <khadkabinisha48@gmail.com>
2025-01-27 14:47:47 +0545

    first commit

---
+++ server.ts
@@ -0,0 +1,14 @@
+import app from "./src/app";
+
+import { envConfig } from "./src/config/config";
+
+function startServer(){
+
+const port = envConfig.port || 4000
+
+app.listen(envConfig.port,()=>{
+  console.log(`server has started at port[${port}]`)
+})
+}
+
+startServer()
