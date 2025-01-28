cc98bcc5bdf2afbcf9b3687b4cfa37720a97c2b6
sovitha <khadkabinisha48@gmail.com>
2025-01-28 16:26:45 +0545

    Initial commit

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,9 @@
 import express from 'express'
+import  './database/connection'
 const app = express()
+
+
+
+
 
 export default app
---
+++ src/database/connection.ts
@@ -0,0 +1,21 @@
+import { Sequelize } from "sequelize-typescript"
+import { envConfig } from "../config/config"
+
+
+const sequelize=new Sequelize(envConfig.connectionString as string)
+
+try{
+  sequelize.authenticate()
+  .then(()=>{
+console.log("milyo hai authentication!!")
+  })
+
+  .catch(err=>{
+    console.log("error aayo",err)
+  })
+
+}
+catch (error){
+console.log(error)
+}
+export default sequelize
